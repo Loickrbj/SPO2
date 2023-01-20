ce50aa41fa4fa3e3fbf7b12eacbd916297a0655b
Meriem <meriem.tedjini@hotmail.com>
2023-01-20 16:55:39 +0100

    WIP Game Master Interface

---
+++ Assets/Scripts/UI/GameMasterUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMasterUI : MonoBehaviour
+{
+    #region Properties
+
+    #endregion
+
+    #region Attributes
+    PlayersListUI playerListPanel;
+    DataListUI dataPanel;
+    GameObject messagePanel;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
